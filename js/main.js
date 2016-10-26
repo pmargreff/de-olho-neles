a3ff1ef4af74246280eeedde177bde5df4b80f70
pmargreff <pmargreff@gmail.com>
2016-10-26 15:41:21 -0200

    init commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var myApp = angular.module('serenata-de-amor-visualization', ['ui.router']);
+
+myApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  
+  $urlRouterProvider.otherwise("/");
+  
+  $locationProvider.html5Mode({
+    enabled: false,
+    requireBase: false,
+    rewriteLinks: false
+  });
+  
+  $stateProvider
+    .state('view1', {
+      url: "/",
+      templateUrl: "partials/view1.html"
+    })  
+    .state('view2', {
+      url: "/view2",
+      templateUrl: "partials/view2.html"
+    })  
+});
---
+++ script/groupby.jl
@@ -0,0 +1,42 @@
+using DataFrames
+
+function groupbystate(df, name)
+  groupdf = by(df, [:state, :month], df -> sum(df[:net_value]))
+  
+  outputfile = string("data/",name,"bystate.csv")
+
+  writetable(outputfile, groupdf)
+
+end
+
+function groupbyperson(df, name)
+  groupdf = by(df, [:congressperson_name], df -> sum(df[:net_value]))
+  
+  outputfile = string("data/",name,"byperson.csv")
+
+  writetable(outputfile, groupdf)
+
+end
+
+function groupbypersonandsubquota(df, name)
+  groupdf = by(df, [:congressperson_name, :subquota_description], df -> sum(df[:net_value]))
+  
+  outputfile = string("data/",name,"bypersonandsubquota.csv")
+
+  writetable(outputfile, groupdf)
+
+end
+
+
+function main()
+  inputfile = "2015"
+  inputpath = string("data/",inputfile,".csv")
+  
+  df = readtable(inputpath)
+  
+  groupbystate(df, inputfile)
+  groupbyperson(df, inputfile)
+  groupbypersonandsubquota(df, inputfile)
+end
+
+main()
