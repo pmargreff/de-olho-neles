9281b1ff3bb48244a499bbc75dd277aa2c622697
pmargreff <pmargreff@gmail.com>
2016-11-06 02:54:45 -0200

    feature:
    loading party data async

--- js/party.js
+++ js/party.js
@@ -1,66 +1,69 @@
-init()
 
-function init (){
+d3 = d3version4;
+
+d3.queue()
+.defer(d3.csv, '/data/parties.csv')
+.defer(d3.csv, '/data/byparty.csv')
+.await(init);
+
+function init (error, parties, byparty){
+  d3 = d3version4;
+  var parseTime = d3.timeParse("%Y-%m");
+  d3 = d3version3;
+
+  party = parties;
+  data = byparty;
   parties = [];
-  d3.csv("/data/parties.csv", function(party) {
-    for (var i = 0; i < party.length; i++){
-      var obj = {
-        values: [], 
-        key: party[i].party,
-        color: getRandomColor()
-      };
-      parties.push(obj)
-    }
-    
-    d3.csv("/data/byparty.csv", function(data) {
-      for (var i = 0; i < parties.length; i++){
-        values = [];
-        for (var j = 0; j < data.length; j++) {
-          if (parties[i].key == data[j].party) {
-            values.push({x: parseTime(data[j].year + "-" + data[j].month), y: parseInt(parseFloat(data[j].x1))})
-          }
-        }
-        parties[i].values = values;
+  for (var i = 0; i < party.length; i++){
+    var obj = {
+      values: [], 
+      key: party[i].party,
+      color: getRandomColor()
+    };
+    parties.push(obj)
+  }
+  
+  for (var i = 0; i < parties.length; i++){
+    values = [];
+    for (var j = 0; j < data.length; j++) {
+      if (parties[i].key == data[j].party) {
+        values.push({x: parseTime(data[j].year + "-" + data[j].month), y: parseInt(parseFloat(data[j].x1))})
       }
+    }
+    parties[i].values = values;
+  }
+  
+  nv.addGraph(function() {
+    chart = nv.models.lineChart()
+    .options({
+      duration: 300,
+      useInteractiveGuideline: true
+    })
+    ;
+    // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
+    chart.xAxis
+    .axisLabel("Date - Year-Month")
+    .tickFormat(function(d) { return d3.time.format('%Y-%m')(new Date(d)); })
+    .staggerLabels(true)
+    ;
+    chart.yAxis
+    .axisLabel('Value - R$')
+    .tickFormat(function(d) {
+      if (d == null) {
+        return 'N/A';
+      }
+      return d3.format(',.2f')(d);
     });
     
-    var chart;
-    var data;
-    d3 = d3version4;
-    var parseTime = d3.timeParse("%Y-%m");
-    d3 = d3version3;
-    nv.addGraph(function() {
-      chart = nv.models.lineChart()
-      .options({
-        duration: 300,
-        useInteractiveGuideline: true
-      })
-      ;
-      // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
-      chart.xAxis
-      .axisLabel("Date - Year-Month")
-      .tickFormat(function(d) { return d3.time.format('%Y-%m')(new Date(d)); })
-      .staggerLabels(true)
-      ;
-      chart.yAxis
-      .axisLabel('Value - R$')
-      .tickFormat(function(d) {
-        if (d == null) {
-          return 'N/A';
-        }
-        return d3.format(',.2f')(d);
-      });
-      
-      data = parties;
-
-      
-      d3.select('#party-svg')
-      .datum(data)
-      .call(chart);
-      nv.utils.windowResize(chart.update);
-      return chart;
-    });
-  });  
+    data = parties;
+    
+    
+    d3.select('#party-svg')
+    .datum(data)
+    .call(chart);
+    nv.utils.windowResize(chart.update);
+    return chart;
+  });
 }
 
 function getRandomColor() {
